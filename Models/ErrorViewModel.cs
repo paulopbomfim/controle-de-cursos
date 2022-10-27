34dce9d27e71cdd5892d524c7dc39a8d7d825213
Paulo Henrique Bomfim <paulopbomfim@gmail.com>
2022-10-27 10:05:09 -0300

    Intens removidos

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace controle_de_cursos.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
